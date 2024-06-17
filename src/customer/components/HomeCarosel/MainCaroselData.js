13796b158bc5edccd44002a8e2ddc002d2c39adc
hhieun1604 <convitne722@gmail.com>
2024-06-17 21:16:17 +0700

    feat: new Main and Honepage

---
+++ src/customer/components/HomeCarosel/MainCaroselData.js
@@ -0,0 +1,22 @@
+export const mainCarouselData=[
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        path:"/women/clothing/women_dress"
+    },
+    {
+        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        path:"/women/clothing/women_dress"
+    }
+]
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MainCarosel from '../../components/HomeCarosel/MainCarosel'
+
+const HomePage = () => {
+  return (
+    <div>
+        <MainCarosel/>
+
+        <div>
+            Hiếu bé
+        </div>
+
+    </div>
+  )
+}
+
+export default HomePage
